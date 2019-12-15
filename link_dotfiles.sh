c5df7ca50ec640184c6a2a32576fa7108f18eead
Jacek Krasiukianis <jacekkra@users.noreply.github.com>
2019-12-15 18:09:15 +0100

    Support platform-specific VSCodium config dirs

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+case $(uname -s) in
+	Darwin)
+		VSCODIUM_CONFIG_DIR=$HOME/Library/Application\ Support/VSCodium/User
+		;;
+	Linux)
+		VSCODIUM_CONFIG_DIR=$HOME/.config/VSCodium/User
+		;;
+	*)
+		echo "Unknown platform"
+		exit 1
+esac
+
+
 ln -sf $PWD/vimrc $HOME/.vimrc
 ln -sf $PWD/zshrc $HOME/.zshrc
 
@@ -11,6 +24,5 @@ NVIM_CONFIG_DIR=$HOME/.config/nvim
 mkdir -p "$NVIM_CONFIG_DIR"
 ln -sf $PWD/init.vim "$NVIM_CONFIG_DIR"/init.vim
 
-VSCODIUM_CONFIG_DIR=$HOME/Library/Application\ Support/VSCodium/User
 mkdir -p "$VSCODIUM_CONFIG_DIR"
 ln -sf $PWD/settings.json "$VSCODIUM_CONFIG_DIR"/settings.json
