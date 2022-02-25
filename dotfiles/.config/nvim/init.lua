b718b37367a561d253e311e7626b9decbe52b0cb
Jacek Krasiukianis <54960015+jacekkra@users.noreply.github.com>
2022-02-25 16:07:15 +0100

    Upgrade from init.vim to init.lua

---
+++ dotfiles/.config/nvim/init.lua
@@ -0,0 +1,7 @@
+vim.opt.autowrite = true
+vim.opt.clipboard = "unnamed"
+vim.opt.ignorecase = true
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.smartcase = true
+
--- dotfiles/.config/nvim/init.vim
+++
@@ -1,6 +0,0 @@
-set autowrite
-set clipboard=unnamed
-set ignorecase
-set mouse=a
-set number
-set smartcase
